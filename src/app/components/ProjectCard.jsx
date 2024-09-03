bb1bd9169cd0dd63bac25fcb83a5cc466ec02772
Enrico Zamarripa <ricozama121803@gmail.com>
2024-09-03 16:56:41 -0700

    changes

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -13,18 +13,22 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank" 
+            rel="noopener noreferrer"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank" 
+            rel="noopener noreferrer"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
